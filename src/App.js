6011eb7d9850595bfd7ca0b911db7c92b099908c
brufeiromero <brufeli.romero@gmail.com>
2022-09-03 12:13:37 -0500

    contador dentro del ItemCount

--- src/App.js
+++ src/App.js
@@ -49,33 +49,11 @@ function App() {
 
   }, [])
 
-  const sumarPruducto = () =>{
-    // console.log('hiciste click')
-    console.log(stoke);
-    if(counter < 5){
-      setCounter(counter + 1)
-    }
-  }
-
-  const restarProducto = () =>{
-    // console.log('hiciste click')
-    if(counter != 1){
-      setCounter(counter - 1)
-    }
-  }
-
-  const stoke = 5;
-
+  
   return (
     <div>
       <NavBar valorCounter={counter}/>
-      <div className='m-5'>
-        <h3>Producto Tienda</h3>
-        <button onClick={restarProducto} className='btn m-5'>-</button>
-        <strong>Contador: {counter}</strong>
-        <button onClick={sumarPruducto} className='btn m-5'>+</button>
-      </div>
-      
+         
       
       <br />
       
--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { useState } from 'react';
+
 
 const ItemCount = ( { stock, initial,  onAdd } ) => {
+
+  const [counter, setCounter] = useState(1)
+
+
+  const sumarPruducto = () =>{
+    // console.log('hiciste click')
+    console.log(stoke);
+    if(counter < 5){
+      setCounter(counter + 1)
+    }
+  }
+
+  const restarProducto = () =>{
+    // console.log('hiciste click')
+    if(counter != 1){
+      setCounter(counter - 1)
+    }
+  }
+
+  const stoke = 5;
+
+
   return (
-    <div className='bg-base-300'>
-        
-    </div>
+    <div className='m-5'>
+        <h3>Producto Tienda</h3>
+        <button onClick={restarProducto} className='btn m-5'>-</button>
+        <strong>Contador: {counter}</strong>
+        <button onClick={sumarPruducto} className='btn m-5'>+</button>
+      </div>
   )
 }
 
